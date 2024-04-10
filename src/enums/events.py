3579ac9de02b2b88be3382eb48cba4c866c01ad9
Eldos <eldos.baktybekov@gmail.com>
2024-04-10 10:36:35 +0600

    :recycle: Rename enum

--- src/enums/events.py
+++ src/enums/events.py
@@ -1,7 +1,7 @@
 from enum import StrEnum
 
-__all__ = ('SpecificChatsEventType',)
+__all__ = ('EventType',)
 
 
-class SpecificChatsEventType(StrEnum):
+class EventType(StrEnum):
     LATE_DELIVERY_VOUCHERS = 'LATE_DELIVERY_VOUCHERS'
