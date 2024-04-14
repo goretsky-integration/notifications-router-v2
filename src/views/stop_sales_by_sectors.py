60e1aab84a5d45b41ebdd9f588392d33fbad43c8
Eldos <eldos.baktybekov@gmail.com>
2024-04-14 23:11:04 +0600

    :lipstick: Add stop sale by sector view

---
+++ src/views/stop_sales_by_sectors.py
@@ -0,0 +1,19 @@
+from models import UnitStopSalesBySectors
+from views.base import View
+
+__all__ = ('UnitStopSaleBySectorsView',)
+
+
+class UnitStopSaleBySectorsView(View):
+
+    def __init__(self, unit_stop_sales_by_sectors: UnitStopSalesBySectors):
+        self.__unit_stop_sales_by_sectors = unit_stop_sales_by_sectors
+
+    def get_text(self) -> str:
+        lines = [f'<b>{self.__unit_stop_sales_by_sectors.unit_name}</b>']
+        for stop_sale in self.__unit_stop_sales_by_sectors.stop_sales:
+            lines.append(
+                f'Сектор: {stop_sale.sector_name}'
+                f' - с {stop_sale.started_at:%H:%M}'
+            )
+        return '\n'.join(lines)
