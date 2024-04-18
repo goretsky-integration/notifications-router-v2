d7e33e089f55df057c7538d674a3a42cb2035ad8
Eldos <eldos.baktybekov@gmail.com>
2024-04-18 23:07:44 +0600

    :wrench: Add sentry config

--- src/config.py
+++ src/config.py
@@ -8,9 +8,17 @@ __all__ = ('Config', 'get_config', 'CONFIG_FILE_PATH')
 CONFIG_FILE_PATH = pathlib.Path(__file__).parent.parent / 'config.toml'
 
 
+class SentryConfig(BaseModel):
+    is_enabled: bool
+    dsn: SecretStr
+    traces_sample_rate: float
+    profiles_sample_rate: float
+
+
 class Config(BaseModel):
     units_storage_base_url: HttpUrl
     telegram_bot_token: SecretStr
+    sentry: SentryConfig
 
 
 def get_config() -> Config:
@@ -22,4 +30,10 @@ def get_config() -> Config:
             config['external_services_api']['units_storage_base_url']
         ),
         telegram_bot_token=config['telegram_bot']['token'],
+        sentry=SentryConfig(
+            is_enabled=config['sentry']['is_enabled'],
+            dsn=config['sentry']['dsn'],
+            traces_sample_rate=config['sentry']['traces_sample_rate'],
+            profiles_sample_rate=config['sentry']['profiles_sample_rate'],
+        ),
     )
