f0da74412d43efb4d2e31a7dec81927c7d99854c
Eldos <eldos.baktybekov@gmail.com>
2024-04-19 09:43:34 +0600

    :label: Add StopSaleBySalesChannel model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,7 @@
 from .events import *
 from .late_delivery_vouchers import *
 from .report_types import *
+from .shop_sales_by_sales_channels import *
 from .stop_sales_by_ingredients import *
 from .stop_sales_by_sectors import *
 from .write_offs import *
